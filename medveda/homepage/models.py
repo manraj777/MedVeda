3d0cd6486b3e9b58f5f145e632b17f9135410305
Suryansh Sharma <suryansharma09@gmail.com>
2025-04-13 20:15:00 +0530

    added models for testimonials

--- medveda/homepage/models.py
+++ medveda/homepage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# future update
+class Testimonial(models.Model):
+    name = models.CharField(max_length=50)
+    avatar = models.URLField(blank=True)
+    comment = models.TextField()
+    rating = models.PositiveSmallIntegerField(default=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class NewsletterSubscription(models.Model):
+    email = models.EmailField(unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+
+
