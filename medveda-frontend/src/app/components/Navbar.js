ad6dbb5c17a63f5dcc9a9c8a790148e02b3a3ad5
manraj777 <manraj.engg@gmail.com>
2025-04-17 01:41:57 +0530

    home page

--- medveda-frontend/src/app/components/Navbar.js
+++ medveda-frontend/src/app/components/Navbar.js
@@ -1,21 +1,24 @@
+import '../styles/Navbar.css'; // Import your CSS file for custom styles
 export default function Navbar() {
     return (
-      <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
-        {/* 🔶 Logo / App Name */}
-        <div className="text-2xl font-bold text-green-700">
-          MedVeda
+      <div className="fixed w-full bg-white/95 backdrop-blur-sm z-50 border-b border-gray-100 bg-white">
+        <div className='navbar-container px-4'>
+        <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center flex h-20">
+          {/* 🔶 Logo / App Name */}
+          <div className="text-2xl font-bold text-green-700 flex items-center">
+            <span class="text-2xl font-bold text-emerald-800 cursor-pointer">Med<span class="text-amber-600">Veda</span></span>
+          </div>
+    
+          {/* 🔗 Navigation Links */}
+          <div className="space-x-8 items-center text-sm font-medium text-gray-700 hidden md:flex">
+            <a href="#remedies" className="transition text-gray-600 hover:text-emerald-700">Remedies</a>
+            <a href="#how-it-works" className="text-gray-600 hover:text-emerald-700 transition">How It Works</a>
+            <a href="#testimonials" className="text-gray-600 hover:text-emerald-700 transition">Testimonials</a>
+            <button class="bg-emerald-700 text-white px-6 py-2 rounded-full hover:bg-emerald-800">Sign In</button>
+          </div>
+        </nav>
         </div>
-  
-        {/* 🔗 Navigation Links */}
-        <div className="space-x-6  items-center text-sm font-medium text-gray-700">
-          <a href="#remedies" className="hover:text-green-600 transition">Remedies</a>
-          <a href="#how-it-works" className="hover:text-green-600 transition">How It Works</a>
-          <a href="#testimonials" className="hover:text-green-600 transition">Testimonials</a>
-          <a href="/signin" className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition">
-            Sign In
-          </a>
-        </div>
-      </nav>
+      </div>
     );
   }
   
--- medveda-frontend/tailwind.config.js
+++ medveda-frontend/tailwind.config.js
@@ -2,11 +2,12 @@ module.exports = {
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
-      "./app/**/*.{js,ts,jsx,tsx}" // If using /app router
+      "./app/**/*.{js,ts,jsx,tsx}",// If using /app router
+      "./src/**/*.{html,js}",
+      "./src/**/*.{html,js,ts,jsx,tsx}"
     ],
     theme: {
       extend: {},
     },
     plugins: [],
   }
-  
